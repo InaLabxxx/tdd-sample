6b11225e45aef262907317f270cf3932223ae1b0
InadomeLabo <t.inadome1985.rd@gmail.com>
2020-09-09 01:31:17 +0900

    これは失敗するはず

---
+++ src/tddbc/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package tddbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		fail("これは失敗するはず");
+	}
+
+}
